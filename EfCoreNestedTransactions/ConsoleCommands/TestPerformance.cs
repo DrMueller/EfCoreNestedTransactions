0fbee16c4080c2e79c662ae5433fee3bc5878c17
Matthias Müller <matthias.mueller@trivadis.com>
2020-07-27 13:06:12 +0200

    WIP

--- EfCoreNestedTransactions/ConsoleCommands/TestPerformance.cs
+++ EfCoreNestedTransactions/ConsoleCommands/TestPerformance.cs
@@ -10,8 +10,7 @@ namespace EfCoreNestedTransactions.ConsoleCommands
 {
     public class TestPerformance : IConsoleCommand
     {
-        private const int AmountOfEntities = 1000000;
-
+        private const int AmountOfEntities = 100000;
         private readonly IConsoleWriter _consoleWriter;
         public string Description { get; } = "Test Performance";
         public ConsoleKey Key { get; } = ConsoleKey.F3;
@@ -26,26 +25,67 @@ namespace EfCoreNestedTransactions.ConsoleCommands
             Stopwatch stopwatch;
 
             await CleanDbAsync();
-
             await using (var db = new BloggingContext())
             {
                 stopwatch = Stopwatch.StartNew();
-                Enumerable.Range(0, 1000000).ForEach(_ => db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" }));
+                Enumerable.Range(0, AmountOfEntities).ForEach(_ => db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" }));
                 await db.SaveChangesAsync();
                 stopwatch.Stop();
-                _consoleWriter.WriteLine($"Insert {AmountOfEntities} blogs without custom transaction, 1 SaveChanges: {stopwatch.Elapsed.Seconds} seconds");
+                _consoleWriter.WriteLine($"Inserting {AmountOfEntities} blogs without custom transaction, 1 SaveChanges: {stopwatch.Elapsed.Seconds} seconds");
             }
 
             await CleanDbAsync();
-
             await using (var db = new BloggingContext())
             {
                 stopwatch = Stopwatch.StartNew();
-                Enumerable.Range(0, 1000000).ForEach(_ => db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" }));
-                await db.SaveChangesAsync();
+                await using (var transaction = await db.Database.BeginTransactionAsync())
+                {
+                    Enumerable.Range(0, AmountOfEntities).ForEach(_ => db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" }));
+                    await db.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                }
+
                 stopwatch.Stop();
-                _consoleWriter.WriteLine($"Insert 100'000 blogs without custom transaction, 1 SaveChanges: {stopwatch.Elapsed.Seconds} seconds");
+                _consoleWriter.WriteLine($"Insert 100'000 blogs with custom transaction, 1 SaveChanges: {stopwatch.Elapsed.Seconds} seconds");
             }
+
+            // This never returns, as it is too slow
+
+            ////await CleanDbAsync();
+            ////await using (var db = new BloggingContext())
+            ////{
+            ////    stopwatch = Stopwatch.StartNew();
+
+            ////    for (var i = 0; i <= AmountOfEntities; i++)
+            ////    {
+            ////        db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            ////        await db.SaveChangesAsync();
+            ////    }
+
+            ////    stopwatch.Stop();
+            ////    _consoleWriter.WriteLine($"Insert 100'000 blogs without custom transaction, 1 SaveChanges per entry: {stopwatch.Elapsed.Seconds} seconds");
+            ////}
+
+            // This also never returns, as it is too slow
+
+            ////await CleanDbAsync();
+            ////await using (var db = new BloggingContext())
+            ////{
+            ////    stopwatch = Stopwatch.StartNew();
+            ////    await using (var transaction = await db.Database.BeginTransactionAsync())
+            ////    {
+            ////        for (var i = 0; i <= AmountOfEntities; i++)
+            ////        {
+            ////            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            ////            await db.SaveChangesAsync();
+            ////        }
+
+            ////        await transaction.CommitAsync();
+            ////    }
+
+            ////    stopwatch.Stop();
+            ////    _consoleWriter.WriteLine($"Insert 100'000 blogs with custom transaction, 1 SaveChanges per entry: {stopwatch.Elapsed.Seconds} seconds");
+            ////}
         }
 
         private async Task CleanDbAsync()
